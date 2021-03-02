df2b7239525fa62a84eb7d14cef322954e06dc4b
caduceus33 <marcrodriguez93@gmail.com>
2021-03-02 04:44:18 +0100

    change router

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x22902B3604CafeA0fCAAe289241Bf20a62F37104'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
