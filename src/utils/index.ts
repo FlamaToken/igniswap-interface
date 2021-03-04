ef06c4808957a097dc31448b095c38bb25f5193b
caduceus33 <marcrodriguez93@gmail.com>
2021-03-04 09:15:11 +0100

    update bscscan

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -32,7 +32,7 @@ export function getEtherscanLink(
   data: string,
   type: 'transaction' | 'token' | 'address' | 'block'
 ): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[97]}bscscan.com`
 
   switch (type) {
     case 'transaction': {
